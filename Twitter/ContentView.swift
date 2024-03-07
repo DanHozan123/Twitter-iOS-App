9cd9aa84481b4760185a168bdeff126ea17c6fff
DanHozan123 <danhozan@gmail.com>
2024-03-07 14:23:52 +0200

    Initial Commit

---
+++ Twitter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Twitter
+//
+//  Created by Dan Hozan on 07.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Dan Hozan on 07.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
